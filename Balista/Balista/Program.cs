04159d777a37fa1727ab24329c7dd4ee1ebd87f7
Wiezerzz <demirijunk@gmail.com>
2015-02-21 11:01:51 +0100

    First Release Balista

--- Balista/Balista/Program.cs
+++ Balista/Balista/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using LeagueSharp;
 using LeagueSharp.Common;
 
@@ -11,14 +8,14 @@ using Color = System.Drawing.Color;
 
 namespace Balista
 {
-    class Program
+    internal class Program
     {
         public static Spell R;
         private static Obj_AI_Hero Player;
         public static Menu menu;
 
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
         }
@@ -27,35 +24,81 @@ namespace Balista
         {
             Player = ObjectManager.Player;
 
-            //check to see if correct champ
-            if (Player.BaseSkinName != "Kalista") return;
+            //Check to see if it is Kalista.
+            if (Player.ChampionName != "Kalista") return;
 
             R = new Spell(SpellSlot.R, 1500f);
             R.SetSkillshot(0.50f, 1500, float.MaxValue, false, SkillshotType.SkillshotCircle);
 
             menu = new Menu("Balista", "Balista", true);
-            menu.AddItem(new MenuItem("useToggle", "Toggle").SetValue(true));
-            menu.AddItem(new MenuItem("useOnComboKey", "Enabled").SetValue(new KeyBind(32, KeyBindType.Press)));
+            {
+                menu.AddItem(new MenuItem("useToggle", "Toggle").SetValue(false));
+                menu.AddItem(new MenuItem("useOnComboKey", "Enabled").SetValue(new KeyBind(32, KeyBindType.Press)));
+            }
+            Menu drawMenu = new Menu("Drawings", "Drawings");
+            {
+                drawMenu.AddItem(new MenuItem("minBRange", "Balista Min Range", true).SetValue(new Circle(false, Color.Chartreuse)));
+                drawMenu.AddItem(new MenuItem("maxBRange", "Balista Max Range", true).SetValue(new Circle(false, Color.Green)));
+            }
+            Menu misc = new Menu("Misc", "misc");
+            {
+                misc.AddItem(new MenuItem("minRange", "Min Range to Balista", true).SetValue(new Slider(700, 100, 1400)));
+                misc.AddItem(new MenuItem("maxRange", "Max Range to Balista", true).SetValue(new Slider(1500, 100, 1500)));
+                misc.AddItem(new MenuItem("usePackets", "Use Packets").SetValue(false));
+            }
 
+            menu.AddSubMenu(drawMenu);
+            menu.AddSubMenu(misc);
             menu.AddToMainMenu();
 
             Game.OnGameUpdate += Game_OnGameUpdate;
             Drawing.OnDraw += Drawing_OnDraw;
-
-            Game.PrintChat("Balista Loaded! - Bruqaj#");
+            Game.PrintChat("<font color='#FF0000'>Balista</font> loaded! - <font color='#5882FA'>Wiezerzz</font>");
         }
 
         private static void Drawing_OnDraw(EventArgs args)
         {
-            throw new NotImplementedException();
+            if (menu.Item("minBRange", true).GetValue<Circle>().Active)
+            Render.Circle.DrawCircle(Player.Position, menu.Item("minRange", true).GetValue<Slider>().Value, menu.Item("minBRange", true).GetValue<Circle>().Color, 3);
+            if (menu.Item("maxBRange", true).GetValue<Circle>().Active)
+                Render.Circle.DrawCircle(Player.Position, menu.Item("maxRange", true).GetValue<Slider>().Value, menu.Item("maxBRange", true).GetValue<Circle>().Color, 3);
         }
 
         private static void Game_OnGameUpdate(EventArgs args)
         {
-            throw new NotImplementedException();
+            if (Player.IsDead || !menu.Item("useToggle").GetValue<bool>() &&
+                !menu.Item("useOnComboKey").GetValue<KeyBind>().Active || !R.IsReady()) return;
+
+            var blitzfriend =
+                ObjectManager.Get<Obj_AI_Hero>()
+                    .SingleOrDefault(
+                        x =>
+                            x.IsAlly && Player.Distance(x.ServerPosition) < menu.Item("maxRange", true).GetValue<Slider>().Value &&
+                            Player.Distance(x.ServerPosition) >= menu.Item("minRange", true).GetValue<Slider>().Value && !x.IsMe &&
+                            x.ChampionName == "Blitzcrank");
+
+            if (blitzfriend == null)
+                return;
+
+            foreach (
+                Obj_AI_Hero enem in
+                    ObjectManager.Get<Obj_AI_Hero>()
+                        .Where(enem => enem.IsValid && enem.IsEnemy && enem.Distance(Player) <= 2450f)) //950f is blitz Q range.
+            {
+                if (enem != null)
+                    foreach (BuffInstance buff in enem.Buffs)
+                    {
+                        if (enem.Buffs != null)
+                        {
+                            if (buff.Name == "rocketgrab2" && R.IsReady())
+                            {
+                                //Game.PrintChat("Grabbed!");
+                                R.Cast(menu.Item("usePackets").GetValue<bool>());
+                            }
+                        }
+                    }
+            }
         }
-
-
 
     }
 }
