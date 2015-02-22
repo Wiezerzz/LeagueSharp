696d0f9abbb0a60aeccc2c4551eaca3654caa989
Wiezerzz <demirijunk@gmail.com>
2015-02-22 11:00:15 +0100

    a little faster

--- Balista/Balista/Program.cs
+++ Balista/Balista/Program.cs
@@ -5,11 +5,21 @@ using LeagueSharp.Common;
 
 using SharpDX;
 using Color = System.Drawing.Color;
+using Menu = LeagueSharp.Common.Menu;
+using MenuItem = LeagueSharp.Common.MenuItem;
 
 namespace Balista
 {
     internal class Program
     {
+        /*
+         * This is my first assembly/script whatever you wanna call it written for LeagueSharp(L#).
+         * This can and will be improved as I progress to learn more about LeagueSharp.Common etc.
+         * I've made a few programs before and I always struggle with the most important thing: Performance.
+         * So if you think one of my assemblies/programs can be improved please contact me. :)
+        */
+
+        public static string BalistaVersion = "1.0.03";
         public static Spell R;
         private static Obj_AI_Hero Player;
         public static Menu menu;
@@ -35,8 +45,8 @@ namespace Balista
             //Check if you have a Blitzfriend :)
             if(!BlitzInGame()) return;
 
-            R = new Spell(SpellSlot.R, 1500f);
-            R.SetSkillshot(0.50f, 1500, float.MaxValue, false, SkillshotType.SkillshotCircle);
+            R = new Spell(SpellSlot.R, 1450);
+            R.SetSkillshot(0.50f, 1450, float.MaxValue, false, SkillshotType.SkillshotCircle);
 
             menu = new Menu("Balista", "Balista", true);
             {
@@ -60,8 +70,8 @@ namespace Balista
             }
             Menu misc = new Menu("Misc", "misc");
             {
-                misc.AddItem(new MenuItem("minRange", "Min Range to Balista", true).SetValue(new Slider(700, 100, 1400)));
-                misc.AddItem(new MenuItem("maxRange", "Max Range to Balista", true).SetValue(new Slider(1400, 100, 1500)));
+                misc.AddItem(new MenuItem("minRange", "Min Range to Balista", true).SetValue(new Slider(700, 100, 1449)));
+                misc.AddItem(new MenuItem("maxRange", "Max Range to Balista", true).SetValue(new Slider(1450, 100, 1500)));
                 misc.AddItem(new MenuItem("usePackets", "Use Packets").SetValue(false));
             }
 
@@ -73,9 +83,9 @@ namespace Balista
             Game.OnGameUpdate += Game_OnGameUpdate;
             Drawing.OnDraw += Drawing_OnDraw;
 
-            Game.PrintChat("<font color='#FF0000'>Balista</font> loaded! - <font color='#5882FA'>Wiezerzz</font>");
+            //This is art.. =.='
+            Game.PrintChat("<font size='21'><b><font color='#f8c906'>B</font><font color='#00bce5'>alista:</font></b></font><font size='17'> v" + BalistaVersion + " <font color='#189108'>loaded!</font> - <font color='#cccccc'>Wiezerzz</font></font>");
         }
-
         private static void Drawing_OnDraw(EventArgs args)
         {
             if (menu.Item("minBRange", true).GetValue<Circle>().Active)
@@ -94,29 +104,22 @@ namespace Balista
                     .SingleOrDefault(
                         x =>
                             x.IsAlly && Player.Distance(x.ServerPosition) < menu.Item("maxRange", true).GetValue<Slider>().Value &&
-                            Player.Distance(x.ServerPosition) >= menu.Item("minRange", true).GetValue<Slider>().Value && !x.IsMe &&
+                            Player.Distance(x.ServerPosition) >= menu.Item("minRange", true).GetValue<Slider>().Value &&
                             x.ChampionName == "Blitzcrank");
 
             if (blitzfriend == null)
                 return;
+
 
             foreach (
                 Obj_AI_Hero enem in
                     ObjectManager.Get<Obj_AI_Hero>()
                         .Where(enem => enem.IsValid && enem.IsEnemy && enem.Distance(Player) <= 2450f)) //950f is blitz Q range.
             {
-                if (enem != null && menu.Item("target" + enem.ChampionName).GetValue<bool>())
-                    foreach (BuffInstance buff in enem.Buffs)
-                    {
-                        if (enem.Buffs != null)
-                        {
-                            if (buff.Name == "rocketgrab2" && R.IsReady())
-                            {
-                                //Game.PrintChat("Grabbed!");
-                                R.Cast(menu.Item("usePackets").GetValue<bool>());
-                            }
-                        }
-                    }
+                if (menu.Item("target" + enem.ChampionName).GetValue<bool>() && enem.HasBuff("rocketgrab2", true) && R.IsReady())
+                {
+                    R.Cast(menu.Item("usePackets").GetValue<bool>());
+                }
             }
         }
 
